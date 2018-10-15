60f74e53bf4b4c655a7cf76ad085cdfea7f3c155
Wenjie Guo <wguo@redhat.com>
2018-10-15 13:51:08 +0800

    move demo process from ear to separate process project

--- workflow-ejb/src/main/java/com/lizard/workflow/demo/KIEStartup.java
+++ workflow-ejb/src/main/java/com/lizard/workflow/demo/KIEStartup.java
@@ -3,17 +3,23 @@ package com.lizard.workflow.demo;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.inject.Inject;
 
 import org.jboss.logging.Logger;
+
+import com.lizard.workflow.processbean.ProcessManagerBean;
 
 @Startup
 @Singleton
 public class KIEStartup {
 
+	//@Inject ProcessManagerBean processManager;
 	@PostConstruct
 	public void start(){
 		Logger log = Logger.getLogger(KIEStartup.class);
 		log.info("---------------KIE workflow startup------------");
+		//Move start process into the sessionbean
+		//processManager.startProcess("MyProcess", null);
 	}
 
 }
---
+++ workflow-ejb/src/main/java/com/lizard/workflow/processbean/ProcessManagerBean.java
@@ -0,0 +1,31 @@
+package com.lizard.workflow.processbean;
+
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.jbpm.kie.services.impl.KModuleDeploymentUnit;
+import org.jbpm.services.api.DeploymentService;
+import org.jbpm.services.api.ProcessService;
+import org.jbpm.services.api.model.DeploymentUnit;
+
+@ApplicationScoped
+public class ProcessManagerBean {
+
+	@Inject
+    private ProcessService processService;
+    @Inject
+    private DeploymentService deploymentService;
+    
+    private String myUnitId = "";
+
+    public void startProcess(final String processId, final Map<String, Object> parameters) {
+        if (deploymentService.getDeployedUnit(myUnitId) == null) {
+            DeploymentUnit unit = new KModuleDeploymentUnit("com.lizard", "workflow-processes-demo", "0.0.1-SNAPSHOT");
+            myUnitId = unit.getIdentifier();
+            deploymentService.deploy(unit);
+        }
+        processService.startProcess(myUnitId, processId, parameters);
+}
+}
